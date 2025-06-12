ab9ca0fa6b849006bc17d788eed2b460d5b64877
MD-Jaini <kmb8074@gmail.com>
2025-06-12 06:54:31 +0530

    Add files via upload

---
+++ New.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    mport React from "react";
+import ReactDOM from "react-dom/client";
+import Welfare-app-final from "./WelfareApp.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Welfare-app-final />);
+
+  );
+};
+
+export default MyComponent;
